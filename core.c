93dab8e6b30ca177091a9342d2eaf3c3fd5a3dff
dreamflyforever <1150660744@qq.com>
2023-07-15 22:59:28 +0800

    add time printf for measure  create pthread
    Signed-off-by: dreamflyforever <1150660744@qq.com>

--- core.c
+++ core.c
@@ -1,6 +1,20 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/time.h>
+#include <sys/timeb.h>
+#include <time.h>
+
+#define DEBUG 1
+
+#if DEBUG
+#define hp_printf(format, ...) \
+	{printf("[%s : %s : %d] ", \
+	__FILE__, __func__, __LINE__); \
+	printf(format, ##__VA_ARGS__);}
+#else
+#define hp_printf(format, ...) 
+#endif
 
 typedef void * (*TASK_ENTRY) (void *p_arg);
 #define BATCH 100
@@ -14,12 +28,14 @@ typedef struct HADNLE {
 int task_create(HANDLE *task_obj, TASK_ENTRY handle_cb, void *data);
 
 int batch_handle(int sum, TASK_ENTRY handle_cb, void *data);
+/*get microsecond*/
+long get_ms();
 
 void * task_logic(void * data)
 {
 	int n = *(int *)data;
 	while (1) {
-		printf("%s %d: %d\n", __func__, __LINE__, n);
+		hp_printf("%d\n", n);
 		sleep(10);
 	}
 }
@@ -31,7 +47,7 @@ int main()
 	ret = batch_handle(BATCH, task_logic, "picture_path");
 	//task_create(&obj, task_logic, "test_argc");
 	while (1) {
-		printf("%s %d\n", __func__, __LINE__);
+		hp_printf("working");
 		sleep(10);
 	}
 	return ret;
@@ -43,6 +59,7 @@ HANDLE patch_obj[BATCH];
 int batch_handle(int sum, TASK_ENTRY cb, void *data)
 {
 	int ret = 0, i;
+	long start = get_ms();
 	for (i = 0; i <= sum; i++) {
 		ret = task_create(&patch_obj[i], cb, (void *)&i);
 
@@ -50,16 +67,37 @@ int batch_handle(int sum, TASK_ENTRY cb, void *data)
 			perror("create pthread error\n");
 		}
 	}
+	long end = get_ms();
+	hp_printf("create phtread time : %ld ms, start: %ld, end: %ld\n", (end - start), start, end);
 	return ret;
 }
 
 int task_create(HANDLE *obj, TASK_ENTRY cb, void *data)
 {
 	int ret;
+	//long start = get_ms();
 	ret = pthread_create(&(obj->ct), NULL, cb, data);
+	//hp_printf("create phtread time : %ld ms\n", (get_ms() - start));
 	if (ret != 0) {
 		perror("create pthread error\n");
 	}
 	
 	return ret;
+}
+
+long get_ms()
+{
+#if 0
+	struct timeval timestamp = {};
+	if (0 == gettimeofday(&timestamp, NULL))
+	    return timestamp.tv_sec * 1000000 + timestamp.tv_usec;
+	else
+	    return 0;
+#endif
+	struct timeb timestamp = {};
+	
+	if (0 == ftime(&timestamp))
+	    return timestamp.time * 1000 + timestamp.millitm;
+	else
+	    return 0;
 }
