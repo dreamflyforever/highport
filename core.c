153e6222c7603eb98a4e393d01aff9e28756ee83
dreamflyforever <1150660744@qq.com>
2023-07-15 10:34:05 +0800

    multi pthread handle batch data
    wip:
         *create multi pthread need more time, because create pthred may malloc
          memory decide to delay system time.
         * maybe data overflow
    
    Signed-off-by: dreamflyforever <1150660744@qq.com>

--- core.c
+++ core.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 
 typedef void * (*TASK_ENTRY) (void *p_arg);
-
+#define BATCH 100
 typedef struct HADNLE {
 	pthread_t ct;
 	TASK_ENTRY cb;
@@ -13,26 +13,41 @@ typedef struct HADNLE {
 
 int task_create(HANDLE *task_obj, TASK_ENTRY handle_cb, void *data);
 
-int patch_handle(int sum, TASK_ENTRY handle_cb, void *data);
+int batch_handle(int sum, TASK_ENTRY handle_cb, void *data);
 
 void * task_logic(void * data)
 {
 	while (1) {
-		printf("%s %d: %s\n", __func__, __LINE__, (char *)data);
-		sleep(5);
+		printf("%s %d: %d\n", __func__, __LINE__, *(int *)data);
+		sleep(10);
 	}
 }
 
 int main()
 {
 	int ret = 0;
-	HANDLE obj;
-	//int ret = patch_handle(100, handle_cb, patch_picture);
-	task_create(&obj, task_logic, "test_argc");
-
+	//HANDLE obj;
+	ret = batch_handle(BATCH, task_logic, "picture_path");
+	//task_create(&obj, task_logic, "test_argc");
 	while (1) {
 		printf("%s %d\n", __func__, __LINE__);
 		sleep(10);
+	}
+	return ret;
+}
+
+HANDLE patch_obj[BATCH];
+
+/*data maybe the per picture path*/
+int batch_handle(int sum, TASK_ENTRY cb, void *data)
+{
+	int ret = 0, i;
+	for (i = 0; i <= sum; i++) {
+		ret = task_create(&patch_obj[i], cb, (void *)&i);
+
+		if (ret != 0) {
+			perror("create pthread error\n");
+		}
 	}
 	return ret;
 }
