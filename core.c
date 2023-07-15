e949305638fb84a3977f4061a8d8dde25e22e105
dreamflyforever <1150660744@qq.com>
2023-07-15 10:18:32 +0800

    add logic handle for phtread task create
    Signed-off-by: dreamflyforever <1150660744@qq.com>

--- core.c
+++ core.c
@@ -1,31 +1,49 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <unistd.h>
 
-typedef void (*TASK_ENTRY) (void *p_arg);
+typedef void * (*TASK_ENTRY) (void *p_arg);
 
 typedef struct HADNLE {
-	pthread_t * context;
+	pthread_t ct;
 	TASK_ENTRY cb;
 	void * data;
 
 } HANDLE;
 
-int task_cleate(HANDLE *task_obj, TASK_ENTRY handle_cb, void *data);
+int task_create(HANDLE *task_obj, TASK_ENTRY handle_cb, void *data);
 
 int patch_handle(int sum, TASK_ENTRY handle_cb, void *data);
 
+void * task_logic(void * data)
+{
+	while (1) {
+		printf("%s %d: %s\n", __func__, __LINE__, (char *)data);
+		sleep(5);
+	}
+}
+
 int main()
 {
-	int ret;
+	int ret = 0;
+	HANDLE obj;
 	//int ret = patch_handle(100, handle_cb, patch_picture);
+	task_create(&obj, task_logic, "test_argc");
+
+	while (1) {
+		printf("%s %d\n", __func__, __LINE__);
+		sleep(10);
+	}
 	return ret;
 }
 
-int task_cleate(HANDLE *task_obj, TASK_ENTRY handle_cb, void *data)
+int task_create(HANDLE *obj, TASK_ENTRY cb, void *data)
 {
 	int ret;
-	ret = pthread_create(task_obj->context, NULL, handle_cb, data);
-
+	ret = pthread_create(&(obj->ct), NULL, cb, data);
+	if (ret != 0) {
+		perror("create pthread error\n");
+	}
+	
 	return ret;
 }
-
