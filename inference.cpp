8157f052b41189b91ea2e20ecebcacc5ee0c73b3
dreamflyforever <1150660744@qq.com>
2023-07-26 19:32:36 +0800

    add inference.cpp
    Signed-off-by: dreamflyforever <1150660744@qq.com>

---
+++ inference.cpp
@@ -0,0 +1,289 @@
+#include <iostream>
+#include <MNN/Interpreter.hpp>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/timeb.h>
+#include <time.h>
+
+#include <sys/types.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#define FILE_NUM_MAX 5000
+#include <opencv2/opencv.hpp>
+
+char file_table[FILE_NUM_MAX][256];
+
+cv::Mat pad(cv::Mat image, std::vector<int> padding, int fill=0, std::string padding_mode="constant") {
+    int pad_left = padding[0];
+    int pad_top = padding[1];
+    int pad_right = padding[2];
+    int pad_bottom = padding[3];
+
+    cv::Mat padded_image;
+    // printf("padding: %d %d %d %d\n", pad_left, pad_top, pad_right, pad_bottom);
+    cv::copyMakeBorder(image, padded_image, pad_top, pad_bottom, pad_left, pad_right, cv::BORDER_CONSTANT, cv::Scalar(fill));
+
+    return padded_image;
+}
+
+cv::Mat rescale(cv::Mat image, int output_size, int interpolation=cv::INTER_LINEAR) {
+    int h = image.rows;
+    int w = image.cols;
+
+    double new_h, new_w;
+    if (output_size > 0) {
+        if (h < w) {
+            new_h = output_size * h / w;
+            new_w = output_size;
+        } else {
+            new_h = output_size;
+            new_w = output_size * w / h;
+        }
+    } else {
+        new_h = output_size;
+        new_w = output_size;
+    }
+
+    cv::Mat resized_image;
+    cv::resize(image, resized_image, cv::Size(static_cast<int>(new_w), static_cast<int>(new_h)), 0, 0, interpolation);
+
+    return resized_image;
+}
+
+cv::Mat rescale_pad(cv::Mat image, int output_size, int interpolation=cv::INTER_LINEAR, int fill=0, std::string padding_mode="constant") {
+    cv::Mat resized_image = rescale(image, output_size, interpolation);
+    // return resized_image;
+    int h = resized_image.rows;
+    int w = resized_image.cols;
+
+    int pad_left = (output_size - w) / 2;
+    int pad_right = output_size - w - pad_left;
+    int pad_top = (output_size - h) / 2;
+    int pad_bottom = output_size - h - pad_top;
+
+    std::vector<int> padding = {pad_left, pad_top, pad_right, pad_bottom};
+
+    cv::Mat padded_image = pad(resized_image, padding, fill, padding_mode);
+
+    return padded_image;
+}
+
+int set_table(char * path)
+{
+	int fn;
+	struct dirent *ptr;
+
+	DIR *dir = opendir(path);
+	int i = 0;
+	while ((ptr = readdir(dir)) != NULL) {
+		if(strcmp(".",ptr->d_name) ==0 || strcmp("..",ptr->d_name) ==  0 || strcmp(" ",ptr->d_name) ==  0) {
+			continue;
+		}
+		//printf("d_name: %s\n", ptr->d_name);
+		//memcpy(file_table[i], ptr->d_name, ptr->d_reclen);
+		sprintf(file_table[i], "%s/%s", path, ptr->d_name);
+		i++;
+	}
+	fn = i;
+#if 0
+	for (; i > 0; i--) {
+		printf("table: %s\n", file_table[i]);
+	}
+#endif
+	/*TODO: if file number > FILE NUMBER MAX*/
+	closedir(dir);
+	return fn;
+}
+#define DEBUG 1
+
+#if DEBUG
+#define hp_printf(format, ...) \
+       {printf("[%s : %s : %d] ", \
+       __FILE__, __func__, __LINE__); \
+       printf(format, ##__VA_ARGS__);}
+#else
+#define hp_printf(format, ...)
+#endif
+
+using namespace MNN;
+
+long get_ms()
+{
+#if 0
+	struct timeval timestamp = {};
+	if (0 == gettimeofday(&timestamp, NULL))
+	    return timestamp.tv_sec * 1000000 + timestamp.tv_usec;
+	else
+	    return 0;
+#endif
+	struct timeb timestamp = {};
+	
+	if (0 == ftime(&timestamp))
+	    return timestamp.time * 1000 + timestamp.millitm;
+	else
+	    return 0;
+}
+char file_buf[2000000] = {0};
+char tmpbuf[256];
+FILE *g_fp;
+int main(int argc, char *argv[])
+{
+    long end, start, inf_sum = 0;
+    long g_start = get_ms();
+	if (argc != 3) {
+		hp_printf("usage : ./main model dir_picture\n");
+		return 0;
+	}
+    start = g_start;
+	g_fp = fopen("./result.txt", "w");
+	if (g_fp == NULL) {
+		perror("fopen error\n");
+		assert(0);	
+	}
+    // const char *pchPath = "/home/pi/MNN/build/best_pref_607.mnn";
+    //const char *pchPath = "/home/pi/MNN/build/s607.mnn";
+    char * pchPath = argv[1];
+    /*interpreter 解释器，是模型数据的持有者，我称之为net
+     * session 会话，是推理数据的持有者，session通过interpreter创建，多个session可以公用一个interpreter
+     * session 和TF的session类似
+     * 我们需要通过net创建session1、session2等会话，然后再给session里送入数据，
+     * 最后通过net->runSession(session)执行推理
+    */
+    std::shared_ptr<Interpreter> net(Interpreter::createFromFile(pchPath));
+
+    // 创建session
+    ScheduleConfig config;
+    config.type  = MNN_FORWARD_AUTO;
+    Session *pSession = net->createSession(config);
+    // 获取输入Tensor
+    // getSessionInput 用于获取单个输入tensor
+    Tensor *ptensorInput = net->getSessionInput(pSession, NULL);
+    std::vector<int> vctInputDims = ptensorInput->shape();
+   
+    // printf("输入Tensor的维度为： ");
+    for (size_t i = 0; i < vctInputDims.size(); ++i)
+    {
+        printf("%d ", vctInputDims[i]);
+    }
+    printf("\n");
+	end = get_ms();
+	hp_printf("create session time : %ld ms\n", (end - start)); 
+	start = end;
+//    shape[0]   = 1;
+    // 重建Tensor的形状，我们这里输入的固定的224*224， 所以可以不需要resize
+//    net->resizeTensor(tensorInput, vctInputShape);
+//    net->resizeSession(pSession);
+    // opencv 读取数据，resize操作，减均值， 除方差，并且转成nchw
+    //char * path = "/media/mclab207/Datas/yc/images_101";
+    char * path = argv[2];
+	int fn = set_table(path);
+	for (int iter = 0; iter < fn; iter++) {
+    cv::Mat matBgrImg = cv::imread(file_table[iter]);
+    cv::Mat matRgbImg;
+    cv::cvtColor(matBgrImg, matRgbImg, cv::COLOR_BGR2RGB);
+    cv::Mat matRgbRescaleImg;
+    matRgbRescaleImg = rescale_pad(matRgbImg, 96);
+    matRgbRescaleImg.convertTo(matRgbRescaleImg, CV_32FC3);
+    cv::Mat matNormImage;
+    cv::Mat matRzRgbImage, matFloatImage;
+    int MODEL_INPUT_HEIGHT = 96;
+    int MODEL_INPUT_WIDTH = 96;
+    cv::Mat matStd(MODEL_INPUT_HEIGHT, MODEL_INPUT_WIDTH, CV_32FC3, cv::Scalar(58.395f, 57.12f, 57.375f));
+    cv::resize(matBgrImg, matRzRgbImage, cv::Size(MODEL_INPUT_WIDTH, MODEL_INPUT_HEIGHT));
+    matRzRgbImage.convertTo(matFloatImage, CV_32FC3);
+    cv::Mat matMean(MODEL_INPUT_HEIGHT, MODEL_INPUT_WIDTH, CV_32FC3, \
+                        cv::Scalar(123.675f, 116.28f, 103.53f)); // 均值
+
+    matNormImage = (matRgbRescaleImg - matMean) / matStd;
+    cv::Mat OnesImage(96, 96, CV_32FC3, cv::Scalar(1.0f, 2.0f, 3.0f));
+    std::vector<std::vector<cv::Mat>> nChannels;
+    std::vector<cv::Mat> rgbChannels(3);
+    cv::split(matNormImage, rgbChannels);
+    nChannels.push_back(rgbChannels); //  NHWC  转NCHW
+    void *pvData = malloc(1 * 3 * MODEL_INPUT_HEIGHT * MODEL_INPUT_WIDTH *sizeof(float));
+    int nPlaneSize = MODEL_INPUT_HEIGHT * MODEL_INPUT_WIDTH;
+    for (int c = 0; c < 3; ++c)
+    {
+        cv::Mat matPlane = nChannels[0][c];
+        memcpy((float *)(pvData) + c * nPlaneSize,\
+             matPlane.data, nPlaneSize * sizeof(float));
+    }
+
+    // 将数据拷贝到Tensor中
+    // 这里采用最简洁的输入方式，直接利用host填充数据，但这种方式仅限于CPU后端，其他后端
+    // 还是需要通过deviceid输入。此外，用这种方式填充数据需要我们自行处理NCHW或者NHWC的数据格式
+    // 本文这里，已经将NHWC转成了NCHW了，即 pvData
+//    std::shared_ptr<MNN::CV::ImageProcess> pretreat_data_ = nullptr;
+    auto nchwTensor = new Tensor(ptensorInput, Tensor::CAFFE);
+    ::memcpy(nchwTensor->host<float>(), pvData, nPlaneSize * 3 * sizeof(float));
+    ptensorInput->copyFromHostTensor(nchwTensor);
+    delete nchwTensor;
+
+	end = get_ms();
+	hp_printf("process picture time : %ld ms\n", (end - start)); 
+	start = end;
+    // 获取输出Tensor
+	long inf_start = get_ms();
+    Tensor *pTensorOutput = net->getSessionOutput(pSession, NULL);
+
+    // 执行推理
+    net->runSession(pSession);
+    {
+        // 获取输出维度类型
+        auto dimType = pTensorOutput->getDimensionType();
+        if (pTensorOutput->getType().code != halide_type_float) {
+            dimType = Tensor::CAFFE;
+        }
+        // 创建输出tensor
+        std::shared_ptr<Tensor> outputUser(new Tensor(pTensorOutput, dimType));
+        // MNN_PRINT("output size:%d\n", outputUser->elementSize());
+        // 拷贝出去
+        pTensorOutput->copyToHostTensor(outputUser.get());
+        auto type = outputUser->getType();
+
+        auto size = outputUser->elementSize();
+        std::vector<std::pair<int, float>> tempValues(size);
+        if (type.code == halide_type_float) {
+            auto values = outputUser->host<float>();
+            for (int i = 0; i < size; ++i) {
+                tempValues[i] = std::make_pair(i, values[i]);
+            }
+        }
+        // Find Max
+        // 排序, 打印
+        std::sort(tempValues.begin(), tempValues.end(),
+                  [](std::pair<int, float> a, std::pair<int, float> b) { return a.second > b.second; });
+
+        int length = size > 10 ? 10 : size;
+        for (int i = 0; i < length; ++i) {
+            MNN_PRINT("%d, %f\n", tempValues[i].first, tempValues[i].second);
+        }
+
+	// sprintf(tmpbuf, "%s %d %f\n", file_table[iter], tempValues[0].first, tempValues[0].second);
+    sprintf(tmpbuf, "%s %d\n", file_table[iter], tempValues[0].first);
+	strcat(file_buf, tmpbuf );
+    }
+    // 释放我们创建的数据内存,这个不是tensor里的
+    if (NULL != pvData)
+    {
+        free (pvData);
+        pvData = NULL;
+    }
+    // std::cout << "hello world " << std::endl;
+
+	end = get_ms();
+	hp_printf("inference & post process time : %ld ms\n", (end - start)); 
+	inf_sum += (end - inf_start);
+}
+
+	fputs(file_buf, g_fp);
+	fclose(g_fp);
+    long g_end = get_ms();
+    hp_printf("all process time : %ld ms, start time: %ld ms, end time: %ld ms \n",
+		 (g_end - g_start), g_start, g_end);
+	hp_printf("inf_time arvg: %f ms\n", (inf_sum/100.));
+    return 0;
+}
+
